d21d473726a7fb905f31ad98b04f90e131787865
Juan Sebastian Paz <juan.paz@safetech.com.co>
2017-02-21 23:07:11 -0500

    Se crea la estructura del programa

--- src/main/java/edu/uniandes/ecos/CAIS/P2PSP1/view/View.java
+++ src/main/java/edu/uniandes/ecos/CAIS/P2PSP1/view/View.java
@@ -1,10 +1,10 @@
-package edu.uniandes.ecos.CAIS.P2PSP1;
+package edu.uniandes.ecos.CAIS.P2PSP1.view;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class View 
 {
     public static void main( String[] args )
     {
